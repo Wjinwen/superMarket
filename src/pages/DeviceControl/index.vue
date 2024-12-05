328d36aa4cd5bd2d645d000aa3d72af5739d0048
Jiin <1319138404@qq.com>
2024-12-05 23:35:28 +0800

    feat:设备控制添加按钮

--- src/pages/DeviceControl/index.vue
+++ src/pages/DeviceControl/index.vue
@@ -1,10 +1,33 @@
 <template>
   <div class="device-control-wrap">
     <div class="device-control"> 
+      <div class="device-control-title">选择门店</div>
+      <div class="device-control-">
+        <searchFiled @search="getSearchData" :showDateRange='false' :showBtn="false" style="margin:0;"/>
+      </div>
+    </div>
+    
+    <div class="device-control"> 
       <div class="device-control-title">启动/关闭总系统</div>
       <div class="device-control-">
         <t-switch v-model="data.sysChecked" size="large">
           <template #label="slotProps">{{ slotProps.value ? '开' : '关' }}</template>
+        </t-switch>
+      </div>
+    </div>
+    <div class="device-control"> 
+      <div class="device-control-title">门锁</div>
+      <div class="device-control-">
+        <t-switch v-model="data.locker" size="large">
+          <template #label="slotProps">{{ slotProps.value ? '开' : '关' }}</template>
+        </t-switch>
+      </div>
+    </div>
+    <div class="device-control"> 
+      <div class="device-control-title">门锁（长期设置）</div>
+      <div class="device-control-">
+        <t-switch v-model="data.lockerSetting" size="large">
+          <template #label="slotProps">{{ slotProps.value ? '常开' : '常关' }}</template>
         </t-switch>
       </div>
     </div>
@@ -36,6 +59,7 @@ export default {
 </script>
 
 <script setup lang="ts">
+import searchFiled from '@/components/searchFiled/index.vue';
 import { computed, nextTick, onDeactivated, onMounted, ref, watch } from 'vue';
 import { SliderProps ,SwitchProps } from 'tdesign-vue-next';
 import { useSettingStore } from '@/store';
@@ -44,11 +68,19 @@ import { LAST_7_DAYS } from '@/utils/date';
 
 
 const store = useSettingStore();
+const storeId=ref<number|null>(null)
 const data = ref({
   sysChecked:true,
+  locker:true,
+  lockerSetting:true,
   soundChecked:true,
   sound:30
 })
+
+const getSearchData = (searchObj:any) => {
+  storeId.value=searchObj.shop
+  // getData()
+};
 
 const onChangeSoundEnd: SliderProps['onChangeEnd'] = (value) => {
   data.value.soundChecked=!!value
